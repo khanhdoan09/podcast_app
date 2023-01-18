74e98b992acf961a3c5b686499952862ee75b667
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2023-01-18 10:59:20 +0700

    export to apk

--- server/src/routes/podcastRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const PodcastService = require("../services/podcastService");
-
-router.get("/getAllPodcastByChannel", async (req, res) =>
-  PodcastService.getAllPodcastByChannel(req, res)
-);
-
-module.exports = router;
